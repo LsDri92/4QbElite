0ce4638b596995cf0df862fb216087cce88a4962
LsDri92 <luciano.s.dri@gmail.com>
2022-08-29 18:20:00 -0300

    Primera Version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CardsComponent } from './components/cards/cards.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HowToComponent } from './components/how-to/how-to.component';
+import { MainComponent } from './components/main/main.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -12,11 +15,19 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     CardsComponent,
-    FooterComponent
+    FooterComponent,
+    HowToComponent,
+    MainComponent
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'loader', component: HowToComponent},
+      {path: 'main', component: MainComponent},
+      {path:'', component: HowToComponent},
+    ]),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
